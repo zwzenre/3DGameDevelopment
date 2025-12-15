8e0f4f6f99c5fa10867ed488b4ef611f59a2a797
Wind <167841372+wind0184@users.noreply.github.com>
2025-12-16 07:20:28 +0800

    Setting 2

--- Assets/Script/CutScene.cs
+++ Assets/Script/CutScene.cs
@@ -5,6 +5,13 @@ public class CutScene : MonoBehaviour
 {
     public string sceneToLoad;
 
+    private void Start()
+    {
+        AudioManager.instance.PlayGameBGM();
+
+    }
+
+
     public void nextScene()
     {
         SceneManager.LoadScene(sceneToLoad);
