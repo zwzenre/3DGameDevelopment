c25586008c0cc9c82b3cc52d0f8ea19ce4b01cfb
Wind <167841372+wind0184@users.noreply.github.com>
2025-12-19 20:06:03 +0800

    Skip CutScene  and fade out

--- Assets/Script/CutScene.cs
+++ Assets/Script/CutScene.cs
@@ -1,19 +1,55 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class CutScene : MonoBehaviour
 {
     public string sceneToLoad;
 
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+
+    private bool isLoading = false;
+
     private void Start()
     {
         AudioManager.instance.PlayGameBGM();
 
+        fadeImage.color = new Color(0, 0, 0, 0);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isLoading)
+        {
+            nextScene();
+        }
+    }
 
     public void nextScene()
     {
+        if (!isLoading)
+        {
+            StartCoroutine(LoadSceneWithFadeOut());
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        float alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += Time.deltaTime / fadeDuration;
+            fadeImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadSceneWithFadeOut()
+    {
+        isLoading = true;
+        yield return StartCoroutine(FadeOut());
         SceneManager.LoadScene(sceneToLoad);
     }
 }
